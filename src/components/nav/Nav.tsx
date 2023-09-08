89fb7c13fa4a244ffdb4f6ffffbf502c3e5d02d8
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-09-08 17:12:04 +0530

    changes made in nav

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,14 +1,21 @@
 import { BsSun } from 'react-icons/bs';
+import desktopDark from '../../assets/desktopDark.jpg';
 
 import './nav.css';
 
 const Nav = () => {
   return (
     <div className='nav-cnt'>
-      <h2 className=''>TODO APP</h2>
-      <button>
-        <BsSun />
-      </button>
+      <div className='navImg-cnt'>
+        <img className='dark-img' src={desktopDark} alt='dark theme' />
+      </div>
+      <div className='nav-txtBtn-cnt'>
+        {' '}
+        <h2 className='nav-hd'>TODO APP</h2>
+        <button>
+          <BsSun />
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -1,4 +1,3 @@
-import desktopDark from '../../assets/desktopDark.jpg';
 import Nav from '../../components/nav/Nav';
 
 import './landingStyle.css';
@@ -6,10 +5,7 @@ import './landingStyle.css';
 const LandingPage = () => {
   return (
     <div className='landingPage-cnt'>
-      <div className='landingFirst-cnt'>
-        <img className='dark-img' src={desktopDark} alt='dark theme' />
-        <Nav />
-      </div>
+      <Nav />
     </div>
   );
 };
