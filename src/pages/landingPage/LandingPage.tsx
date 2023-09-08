09edea7ea03fb4b9c1bede7df6e36fd9da76beb4
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-09-08 15:38:07 +0530

    landingPage created

---
+++ src/pages/landingPage/LandingPage.tsx
@@ -0,0 +1,13 @@
+import desktopDark from '../../assets/desktopDark.jpg';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <div>
+        <img className='darkImg' src={desktopDark} alt='dark theme' />
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
