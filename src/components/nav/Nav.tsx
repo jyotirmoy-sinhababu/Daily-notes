fe12c72b3ac5403eaa1249ff427cb03f69b39199
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-09-08 16:10:26 +0530

    style added

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -2,7 +2,7 @@ import { BsSun } from 'react-icons/bs';
 
 const Nav = () => {
   return (
-    <div className='flex'>
+    <div className='flex justify-center'>
       <h2 className='decoration-gray-300'>TODO APP</h2>
       <button>
         <BsSun />
