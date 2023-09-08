c9ca7fb0cdc5f18e00758f8648dad9e08c71d9b4
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-09-08 15:42:13 +0530

    nav created

---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,14 @@
+import { BsSun } from 'react-icons/bs';
+
+const Nav = () => {
+  return (
+    <div>
+      <h2>TODO APP</h2>
+      <button>
+        <BsSun />
+      </button>
+    </div>
+  );
+};
+
+export default Nav;
