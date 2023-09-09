436e31d32b83c39b8177ab577398c7af71ba4a83
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-09-09 12:57:57 +0530

    redux store and slice created

---
+++ src/slice/Slice.tsx
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const TodoSlice = createSlice({
+  name: 'todos',
+  initialState: {},
+  reducers: {},
+});
+
+export const {} = TodoSlice.actions;
+export default TodoSlice.reducer;
