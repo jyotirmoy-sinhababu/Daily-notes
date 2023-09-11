8c4809306906803f9d987a77987ac1c611a7e263
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-09-11 12:02:13 +0530

    err page created

---
+++ src/components/errorPage/ErrorPage.tsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+  return (
+    <div>
+      <p className='font-black decoration-slate-950 text-4xl'>No data found</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/todoListDisplay/TodoListDisplay.tsx
+++ src/todoListDisplay/TodoListDisplay.tsx
@@ -1,26 +1,44 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteTodo } from '../slice/TodoSlice';
 
+import { useState } from 'react';
+
 import { AiOutlineClose } from 'react-icons/ai';
 
+import ErrorPage from '../components/errorPage/ErrorPage';
+
 const TodoListDisplay = () => {
+  const [isCompleted, setIsCompleted] = useState<boolean>(false);
+
   const todos = useSelector((state: any) => state?.todo.todos);
   const dispatch = useDispatch();
 
   const handleDelete = (id: number) => {
     dispatch(deleteTodo(id));
   };
-
+  console.log(isCompleted);
   return (
     <div className='flex gap-[53px] flex-wrap'>
       {todos ? (
         todos?.map((item: any) => {
           return (
             <div
-              className='border-4 w-[320px] h-[110px] rounded-lg px-2 py-2 bg-slate-700  '
+              className='border-4 w-[320px] h-[110px] rounded-lg px-2 py-2 bg-slate-700 overflow-auto '
               key={item.id}
             >
-              <div className='flex justify-end'>
+              <div className='flex justify-between'>
+                <div>
+                  <input
+                    type='checkbox'
+                    name='completed'
+                    onChange={() => {
+                      !isCompleted
+                        ? setIsCompleted(true)
+                        : setIsCompleted(false);
+                    }}
+                  />
+                  <label>Mark as completed</label>
+                </div>
                 <button
                   type='button'
                   onClick={() => {
@@ -31,7 +49,7 @@ const TodoListDisplay = () => {
                 </button>
               </div>
               <div>
-                <p className=' flex flex-wrap font-semibold text-red-100'>
+                <p className=' flex flex-wrap font-semibold text-red-100 '>
                   {item.dailyTodo}
                 </p>
               </div>
@@ -39,7 +57,9 @@ const TodoListDisplay = () => {
           );
         })
       ) : (
-        <p></p>
+        <div className='flex justify-center'>
+          <ErrorPage />
+        </div>
       )}
     </div>
   );
